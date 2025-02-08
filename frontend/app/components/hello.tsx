a0dde66df13739e7a7aa80fdd5bfbdb8e3e19245
ThiagoWaldrich <thiagowlf7@gmail.com>
2025-02-08 11:31:40 -0300

    some changes

---
+++ frontend/app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Hello(){
+    console.log("Eu sou um componentes");
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+export default Hello;
---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Hello from "./components/hello";
+
+export default function Home() {
+  console.log("O que eu estou fazendo aqui ? -- SERVER/");
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Olá Mundo !</h1>
+      <Hello/>
+    </>
+  );
+}
