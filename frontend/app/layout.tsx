f9d8be8d980724a243a0b21fb6fe262094e6566f
ThiagoWaldrich <thiagowlf7@gmail.com>
2025-02-08 11:58:34 -0300

    Another excellent commit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1 className="text-3-l">ROOT</h1>
         {children}
       </body>
     </html>
